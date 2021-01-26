0f407fcf51049eb25a81149aa397f50f281edac9
StefanOppeneer <stefanoppeneer@gmail.com>
2021-01-26 10:22:13 +0100

    Ik heb alles aangepast naar de 4 data sets

--- Github.R
+++ Github.R
@@ -5,120 +5,6 @@ library(bsts)
 library(hms)
 library(stringr)
 
-
-#
-#       Functions
-#
-#
-
-# #This function finds the mean, median and the 1st and 3rd quartile of the data for every minute, 
-# #where the data have dates in the form "YYYY-MM-DD HH:MM:SS" "D-M-YYYY H:M"
-# XVarsData <- function(data){
-#   
-#   minutes <- 60     #Minutes in hour
-#   hours <- 24       #Hours in day
-#   day <- minutes*hours 
-#   daysInData <- dim(data)[1]/day   #This calculates the number of days in the data
-#   
-#   #Prepare the vectors 
-#   time <- integer(day)
-#   medians <- integer(day)
-#   averages <- integer(day)
-#   quart1 <- integer(day)
-#   quart3 <- integer(day)
-#   inds <- integer(day)
-#   
-#   #Put the time data into the desired format
-#   data$date_time <- GetTime(data$date_time)
-#   
-#   #Need to obtain the x variables for every minute of the day
-#   for (h in 1:hours-1){ #-1, to range from 0 to 23
-#     for (m in 1:minutes-1){ #Logic analogue to hours
-#       hms_code <- 60*m+60*60*h
-#       time_cur <- as_hms(hms_code)
-#       time_cur <- toString(time_cur)  #Stringify the time in the format "HH:MM:00"
-#       time_cur_s <- substr(time_cur,1,5)
-#       data_cur <- data$visits_index[data$date_time==time_cur_s]
-#       data_cur <- sort(data_cur) #Take the visits_index that have a corresponding time representation and sort (deletes NAs)
-#       data_quants <- quantile(data_cur)
-#       
-#       #Add the statistics to the vectors
-#       ind <- 1+(h*60)+m
-#       time[ind] <- time_cur
-#       medians[ind] <- median(data_cur)
-#       averages[ind] <- mean(data_cur)
-#       quart1[ind] <- data_quants[2]
-#       quart3[ind] <- data_quants[4]
-#     }
-#   }
-#   tog <- cbind(time,medians,averages,quart1,quart3)  #make dataframe of the variables combined
-#   tog_df <- as.data.frame(tog)
-#   return (tog_df)
-# }
-# 
-# 
-# Ad_locations <- function(data){
-#   
-#   data_dim = dim(data)[1]
-#   numbs <- 1:data_dim
-#   data["numbs"] <- numbs
-#   
-#   ad_ind <- data$numbs[data$ads==1]
-#   ad_start <- c()
-#   ad_end <- c()
-#   
-#   i <- 1
-#   ad_finished <- 1
-#   while(i<=length(ad_ind)){
-#     if(ad_finished==1){
-#       ad_start <- rbind(ad_start,ad_ind[i])
-#       i = i+7
-#       if (i>length(ad_ind)){
-#         ad_end <- rbind(ad_end,ad_ind[length(ad_ind)])
-#         break
-#       }
-#       if (ad_ind[i]-ad_ind[i-1]>1){
-#         ad_end <- rbind(ad_end,ad_ind[i-1])
-#         ad_finished <- 1
-#       } else{
-#         ad_finished <- 0
-#       }
-#     } else {
-#       i = i+1
-#       if (i>length(ad_ind)){
-#         ad_end <- rbind(ad_end,ad_ind[i-1])
-#         break
-#       }
-#       if (ad_ind[i]-ad_ind[i-1]>1){
-#         ad_end <- rbind(ad_end,ad_ind[i-1])
-#         ad_finished <- 1
-#       } else {
-#         ad_finished <- 0
-#       }
-#     }
-#   }
-#   combined <- as.data.frame(cbind(ad_start,ad_end))
-#   colnames(combined)<-c("start","end")
-#   return (combined)
-# }
-# 
-# #This function converts dates with time into HH:MM
-# GetTime <- function(time){
-#   time <- sub(".* ", "", time)     #Deletes the year/month/day
-#   time_s <- str_split(time[1],":") #Splits HH:MM:SS into separate parts
-#   if(length(time_s[[1]])>2){       #if we have 3 parts, then we need to take the HH:MM part
-#     times <- length(time)
-#     time_new <- integer(times)
-#     for (i in 1:times){
-#       time_new[i] = substr(time[i],1,5)
-#     }
-#   } else{                         #We only have 2 parts, so we already have the HH:MM format which we want
-#     time_new <- time
-#   }
-#   return (time_new)
-# }
-
-
 # 
 #     Script
 #
@@ -129,36 +15,51 @@ path_functions <- "C:/Users/Steef/Documents/Uni/Seminar BA&QM/R code/Seminar/"
 
 source(paste(path_functions,"Functions.R", sep = ""))       #Load in the functions
 
+#Read in the data
+#Make the visit_index when there are ads equal to missing for baseline
 
-data_be <- read.csv(paste(path_data,file="visits_ads_be.csv", sep = ""), header = T)
-data_bayes_be <- data_be    #Make the visit_index when there are ads equal to missing for baseline
-data_bayes_be$visits_index[data_bayes_be$ads==1] <- NaN
-Regressors_be <- XVarsData(data_bayes_be)
+data_be_app <- read.csv(paste(path_data,file="be_app_adj.csv", sep = ""), header = T)
+data_be_app_bayes <- data_be_app
+data_be_app_bayes$visits_index[data_be_app_bayes$ads==1] <- NaN
 
-data_nl <- read.csv(paste(path_data,file="visits_ads_nl.csv", sep = ""), header = T)
-data_bayes_nl <- data_nl    #Make the visit_index when there are ads equal to missing for baseline
-data_bayes_nl$visits_index[data_bayes_nl$ads==1] <- NaN
-Regressors_nl <- XVarsData(data_bayes_nl)
+data_be_web <- read.csv(paste(path_data,file="be_web_adj.csv", sep = ""), header = T)
+data_be_web_bayes <- data_be_web
+data_be_app_bayes$visits_index[data_be_app_bayes$ads==1] <- NaN
 
-numberOfDays <- (dim(data_nl)[1]) / (dim(Regressors_nl)[1])
+data_nl_app <- read.csv(paste(path_data,file="nl_app_adj.csv", sep = ""), header = T)
+data_nl_app_bayes <- data_nl_app
+data_nl_app_bayes$visits_index[data_nl_app_bayes$ads==1] <- NaN
+
+data_nl_web <- read.csv(paste(path_data,file="nl_web_adj.csv", sep = ""), header = T)
+data_nl_web_bayes <- data_nl_web
+data_nl_web_bayes$visits_index[data_nl_web_bayes$ads==1] <- NaN
+
+
+#Create the regressors
+
+Regressors_be_app <- XVarsData(data_be_app_bayes)
+Regressors_be_web <- XVarsData(data_be_web_bayes)
+Regressors_nl_app <- XVarsData(data_nl_app_bayes)
+Regressors_nl_web <- XVarsData(data_nl_web_bayes)
+
+#Find the number of days
+numberOfDays <- (dim(data_nl_web)[1]) / (dim(Regressors_nl_web)[1])
 
 #Duplicate the regressors such that every observation in the data has these variables
-Regressors_nl <- Regressors_nl[rep(rownames(Regressors_nl),numberOfDays),] 
-rownames(Regressors_nl) <- 1:NROW(Regressors_nl)
-Regressors_be <- Regressors_be[rep(rownames(Regressors_be),numberOfDays),]
-rownames(Regressors_be) <- 1:NROW(Regressors_be)
+Regressors_be_app <- Regressors_be_app[rep(rownames(Regressors_be_app),numberOfDays),]
+rownames(Regressors_be_app) <- 1:NROW(Regressors_be_app)
+Regressors_be_web <- Regressors_be_web[rep(rownames(Regressors_be_web),numberOfDays),]
+rownames(Regressors_be_web) <- 1:NROW(Regressors_be_web)
+Regressors_nl_app <- Regressors_nl_app[rep(rownames(Regressors_nl_app),numberOfDays),] 
+rownames(Regressors_nl_app) <- 1:NROW(Regressors_nl_app)
+Regressors_nl_web <- Regressors_nl_web[rep(rownames(Regressors_nl_web),numberOfDays),] 
+rownames(Regressors_nl_web) <- 1:NROW(Regressors_nl_web)
 
-# plot(1:1440,Regressors2$averages,col="red")
-# lines(1:1440,Regressors$averages,col="green")
+#Find the timestamps when ads happen
+ad_locations_be <- Ad_locations(data_be_app)
+ad_locations_nl <- Ad_locations(data_nl_app)
 
-# plot(Regressors$time,Regressors$medians,type="l",col="red")
-# lines(Regressors$time,Regressors$averages,col="green")
-# lines(Regressors$time,Regressors$quart1,col="blue")
-# lines(Regressors$time,Regressors$quart3,col="orange")
-
-ad_locations_be <- Ad_locations(data_be)
-ad_locations_nl <- Ad_locations(data_nl)
-
+#Useful considerations
 day <- 24*60
 week <- day*6
 month <- week*4
@@ -166,25 +67,26 @@ month <- week*4
 Numb_ads_be <- dim(ad_locations_be)[1]
 Numb_ads_nl <- dim(ad_locations_nl)[1]
 
+
 #
 #         FIRST BAYESIAN BASELINE ESTIMATION
 #
 
 end_train_nl <- ad_locations_nl$start[1] - 1
 start_train_nl <- 1
-Regressors_model_nl <- as.matrix(Regressors_nl[start_train_nl:end_train_nl,2:5])
+Regressors_model_nl_web <- as.matrix(Regressors_nl_web[start_train_nl:end_train_nl,2:5])
 
-y_nl <- data_bayes_nl$visits_index[start_train_nl:end_train_nl]
+y_nl_web <- data_nl_web_bayes$visits_index[start_train_nl:end_train_nl]
 #ss_nl <- AddLocalLinearTrend(list(),y_nl)
-ss_nl <- AddAr(list(),y_nl)
-ss_nl <- AddSeasonal(ss_nl,y_nl,nseasons=7,season.duration=day)
-model <- bsts(y_nl~Regressors_model_nl,state.specification = ss_nl,niter=300)
+ss_nl <- AddAr(list(),y_nl_web)
+ss_nl <- AddSeasonal(ss_nl,y_nl_web,nseasons=7,season.duration=day)
+model <- bsts(y_nl_web~Regressors_model_nl_web,state.specification = ss_nl,niter=300)
 
 
-Regressors_pred_nl <- as.matrix(Regressors_nl[ad_locations_nl$start[1]:ad_locations_nl$end[1],2:5])
+Regressors_pred_nl_web <- as.matrix(Regressors_nl_web[ad_locations_nl$start[1]:ad_locations_nl$end[1],2:5])
 f_horizon <- ad_locations_nl$end[1] - end_train_nl
-pred <- predict(model,horizon = f_horizon,newdata = Regressors_pred_nl)
-lift <- data_nl$visits_index[ad_locations_nl$start[1]:ad_locations_nl$end[1]] - pred$mean 
+pred <- predict(model,horizon = f_horizon,newdata = Regressors_pred_nl_web)
+lift <- data_nl_web$visits_index[ad_locations_nl$start[1]:ad_locations_nl$end[1]] - pred$mean 
 lift_tot <- sum(lift)
 print(lift_tot)
 
